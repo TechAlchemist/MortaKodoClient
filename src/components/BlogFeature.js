44304d182541943649af5481bbc289cff1e8019d
Brandon Myers <brandonRmyers1512@gmail.com>
2021-06-22 13:56:15 -0700

    Style updating main page.

--- src/components/BlogFeature.js
+++ src/components/BlogFeature.js
@@ -5,10 +5,14 @@ export default function BlogFeature({ blog }) {
         <div className='card'>
             <div className='card-body'>
                 <h5 className='card-title'> { blog.blogTitle } </h5>
-                <p> Written by: { blog.author }  </p>
-                <p> Date: { new Date(blog.createdAt).toDateString() }  </p>
-                <p> Likes: { blog.likes }  </p>
-                <a className='card-link' href={`/blog/${blog._id}`}> Read Article Now </a>
+                <p> <i className="bi bi-pencil-fill"></i> { blog.author }  </p>
+                <p> <i className="bi bi-calendar-date-fill"></i> { new Date(blog.createdAt).toDateString() }  </p>
+                <p> <i className="bi bi-heart-fill"></i> { blog.likes }  </p>
+                <a className='card-link' href={`/blog/${blog._id}`}> 
+                    <button className='read-more-btn'>
+                        <i className="bi bi-book-fill"></i> Read 
+                    </button> 
+                </a>
                 <hr />
                 {blog.contentTags && blog.contentTags.map((tag, idx) => 
                     <span className="badge bg-info text-dark" key={idx}>{tag} </span>  
